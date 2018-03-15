67b8d3ea7108ff1a3e50e3ffe29aa85a51fd8bbb
Alex Rodrigues <alexcleiton16@gmail.com>
2018-03-15 16:24:09 -0300

    ajuste na busca do paciente

--- www/js/ajax.js
+++ www/js/ajax.js
@@ -1,43 +1,48 @@
-﻿var req;
+﻿var req
 
 function buscarPaciente(valor) {
-	req = new XMLHttpRequest();
+	req = new XMLHttpRequest()
 
-	var url = "http://appcon/json.php?busca="+valor;
+	var url = "http://appcon/json.php?busca="+valor
 
 	req.open("Get", url, true);
 	req.onreadystatechange = function()
 	{
 		if (req.readyState = 1)
 		{
-			document.getElementById('ajax').innerHTML= "<td colspan='4'>Buscando Pacientes...</td>";
+			document.getElementById('ajax').innerHTML= "<td colspan='4'>Buscando Pacientes...</td>"
 		}
 		if (req.readyState = 4 && req.status == 200)
 		{
-			ajax(req.responseText);
+			ajax(req.responseText)
 		}
 	}
 	req.send(null);
 	function ajax(response) {
-		var dados = JSON.parse(response);
-		var nome = document.createTextNode(dados);
-		var html = "<td>";
-		html += nome;
-		html += "</td>";
-		html += "<td>";;
-		html += dados.nome;
-		html += "</td>";
-		html += "<td>";;
-		html += dados.nome;
-		html += "</td>";
-		html += "<td>";;
-		html += dados.nome;
-		html += "</td>";
-		document.getElementById('ajax').innerHTML= html;
+		var dados = response
+		console.log(dados)
+		JSON.parse(dados).forEach( function (registro){
+			var html = "<td>"
+					html += registro.nome
+					html += "</td>"
+					html += "<td>"
+					html += registro.sobrenome
+					html += "</td>"
+					html += "<td>"
+					html += registro.telefone
+					html += "</td>"
+					html += "<td>"
+					html += registro.nome_status
+					html += registro.icons_status
+					html += registro.cor_status
+					html += "</td>"
+					document.getElementById('ajax').innerHTML= html
+		})
+		
 	}
 }
 
 function exibirConteudo(id)
 {
-	req = new XMLHttpRequest();
+	req = new XMLHttpRequest()
 }
