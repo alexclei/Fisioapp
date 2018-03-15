4e6e5ac364d678770e72d2ef607bf569a5173baf
Alex Rodrigues <alexcleiton16@gmail.com>
2018-03-15 20:00:36 -0300

    novos ajustes na busca do paciente

--- www/js/ajax.js
+++ www/js/ajax.js
@@ -8,10 +8,6 @@ function buscarPaciente(valor) {
 	req.open("Get", url, true);
 	req.onreadystatechange = function()
 	{
-		if (req.readyState = 1)
-		{
-			document.getElementById('ajax').innerHTML= "<td colspan='4'>Buscando Pacientes...</td>"
-		}
 		if (req.readyState = 4 && req.status == 200)
 		{
 			ajax(req.responseText)
@@ -20,24 +16,22 @@ function buscarPaciente(valor) {
 	req.send(null);
 	function ajax(response) {
 		var dados = response
-		console.log(dados)
+		var html = ""
 		JSON.parse(dados).forEach( function (registro){
-			var html = "<td>"
-					html += registro.nome
-					html += "</td>"
-					html += "<td>"
-					html += registro.sobrenome
-					html += "</td>"
-					html += "<td>"
-					html += registro.telefone
-					html += "</td>"
-					html += "<td>"
-					html += registro.nome_status
-					html += registro.icons_status
-					html += registro.cor_status
-					html += "</td>"
-					document.getElementById('ajax').innerHTML= html
+			html += "<tr><td>"
+      +registro.nome
+      +"</td><td>"
+      +registro.sobrenome
+      +"</td><td>"
+      +registro.telefone
+      +"</td><td>"
+      +"<div class='btn bg-"+ registro.cor_status +"'>"
+      +"<i class='material-icons'>"+registro.icons_status+"</i>"
+      +"<span>"+registro.nome_status+"</span>"
+      +"</div>"
+      +"</td></tr>"
 		})
+		document.getElementById('ajax').innerHTML = html
 		
 	}
 }
